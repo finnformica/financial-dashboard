a8e74dded8b38892af589e946d1fcf725afd5fd5
finnformica <finnformica@gmail.com>
2023-01-12 14:44:33 +0000

    initialise financial dashboard app

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+// import styles from "../styles/Home.module.css";
+import BasicTabs from "../components/NavTabs/NavTabs";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Dashboard</h1>
+    </>
+  );
+}
