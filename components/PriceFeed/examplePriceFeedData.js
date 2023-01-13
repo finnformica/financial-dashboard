87e4fa7e80711957413cd49168a0c5c84c483849
finnformica <finnformica@gmail.com>
2023-01-13 20:01:37 +0000

    example data for price feed

---
+++ components/PriceFeed/examplePriceFeedData.js
@@ -0,0 +1,43 @@
+export const examplePriceFeedData = [
+  {
+    coin: "Bitcoin",
+    ticker: "BTC",
+    change: 1314,
+    percent: 7.4,
+    price: "18846.88",
+  },
+  {
+    coin: "Ethereum",
+    ticker: "ETH",
+    change: 1314,
+    percent: 2.0,
+    price: "1,418.48",
+  },
+  {
+    coin: "Binance Coin",
+    ticker: "BNB",
+    change: 1314,
+    percent: 0.3,
+    price: "18846.88",
+  },
+  {
+    coin: "XRP",
+    ticker: "XRP",
+    change: 1314,
+    percent: -0.1,
+    price: "0.377271",
+  },
+  {
+    coin: "Cardano",
+    ticker: "ADA",
+    change: 1314,
+    percent: 0.6,
+    price: "0.333865",
+  },
+  {
+    coin: "Dogecoin",
+    ticker: "DOGE",
+    percent: 0.1,
+    price: "0.080353",
+  },
+];
